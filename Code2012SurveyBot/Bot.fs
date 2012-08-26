b4d268adafa1e56fd99540c55bcd8c69a4ac1c25
Sakamoto <junichi.jp@gmail.com>
2012-08-27 00:12:22 +0900

    Combine all.

--- Code2012SurveyBot/Bot.fs
+++ Code2012SurveyBot/Bot.fs
@@ -9,7 +9,7 @@ open Raven.Client.Document
 
     type Reply = {Id:int; Free_Comment:string}
 
-    let replies = 
+    let replies () = 
         let webClient = new WebClient()
         webClient.Encoding <- Encoding.UTF8
         "http://code-survey.herokuapp.com/surveys.json"
@@ -18,28 +18,35 @@ open Raven.Client.Document
             |> Array.filter (fun x -> System.String.IsNullOrEmpty(x.Free_Comment) = false)
             |> Array.sortBy (fun x -> x.Id) 
 
-    type RavenConnSetting = {Url:string; ApiKey:string}
+    type LastTweet = {RepId:int}
 
     let Tweet () = 
+        let reps = replies()
+        let connSetting = ConfigurationManager.AppSettings.["RAVENHQ_CONNECTION_STRING"].Split(';').ToDictionary((fun (x:string) -> x.Split('=').First()), (fun (x:string) -> x.Split('=').Last()))
+        use docStore = new DocumentStore()
+        docStore.Url <- connSetting.["Url"]
+        docStore.ApiKey <- connSetting.["ApiKey"]
+        use session = docStore.Initialize().OpenSession()
         
-//        let connSetting = JsonConvert.DeserializeObject<RavenConnSetting>(ConfigurationManager.AppSettings.["RAVENHQ_CONNECTION_STRING"])
-//        use docStore = new DocumentStore()
-//        docStore.Url <- connSetting.Url
-//        docStore.ApiKey <- connSetting.ApiKey
-//        use session = docStore.Initialize().OpenSession()
-//        
-//        let lastTweets = session.Query<Reply>().ToArray()
-//        let nextTweetTo = 
-//            if lastTweets.Any() then
-//                let nexts = replies.Where(fun x -> x.Id > lastTweets.First().Id)
-//                if nexts.Any() then nexts.First() else replies.First()
-//            else
-//                replies.First()
-//
-//        nextTweetTo
+        let lastTweets = session.Query<LastTweet>().ToArray()
+        let nextTweetTo = 
+            if lastTweets.Any() then
+                let lastTweet = lastTweets.First()
+                session.Delete(lastTweet)
+                let nexts = reps.Where(fun x -> x.Id > lastTweet.RepId).ToArray()
+                if nexts.Any() then nexts.First() else reps.First()
+            else
+                reps.First()
+        session.Store({RepId = nextTweetTo.Id})
+        session.SaveChanges()
 
+        let hashtag = ConfigurationManager.AppSettings.["hashtag"]
+        let msg  = nextTweetTo.Free_Comment
+        let msg' = msg.Substring(0, min (msg.Length) (140 - 1 - hashtag.Length)) + " " + hashtag
 
         let key = ConfigurationManager.AppSettings.["twitterKey"]
         let tokens = JsonConvert.DeserializeObject<OAuthTokens>(key)
-        TwitterStatus.Update(tokens, "AppHarbor 上からもツイートできるだろうか。") |> ignore
+        TwitterStatus.Update(tokens, msg') |> ignore
+
+
 
