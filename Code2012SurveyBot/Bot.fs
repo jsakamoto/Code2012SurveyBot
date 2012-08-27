4c5a54a5269d568195cee30f5b9b802bfd82dafb
Sakamoto <junichi.jp@gmail.com>
2012-08-27 09:27:15 +0900

    Quote and append lead to tweet message.

--- Code2012SurveyBot/Bot.fs
+++ Code2012SurveyBot/Bot.fs
@@ -10,6 +10,7 @@ open Raven.Client.Document
 
     type Reply = {Id:int; Free_Comment:string}
 
+    /// Retrieve free commnets from "Code 2012 Survey" site.
     let replies () = 
         let webClient = new WebClient()
         webClient.Encoding <- Encoding.UTF8
@@ -21,15 +22,19 @@ open Raven.Client.Document
 
     type LastTweet = {RepId:int}
 
+    /// Tweet free commnets of "Code 2012 Survey" sequencialy.
     let Tweet () = 
-        let reps = replies()
+
+        // Connect to RavenDB and retrieve the reply id of last tweet.
         let connSetting = ConfigurationManager.AppSettings.["RAVENHQ_CONNECTION_STRING"].Split(';').ToDictionary((fun (x:string) -> x.Split('=').First()), (fun (x:string) -> x.Split('=').Last()))
         use docStore = new DocumentStore()
         docStore.Url <- connSetting.["Url"]
         docStore.ApiKey <- connSetting.["ApiKey"]
         use session = docStore.Initialize().OpenSession()
-        
         let lastTweets = session.Query<LastTweet>().ToArray()
+
+        // Chose the message to next tweet and store to RavenDB.
+        let reps = replies()
         let nextTweetTo = 
             if lastTweets.Any() then
                 let lastTweet = lastTweets.First()
@@ -41,13 +46,18 @@ open Raven.Client.Document
         session.Store({RepId = nextTweetTo.Id})
         session.SaveChanges()
 
+        // Pre prpcess to tweet.(Appned hash tag, quote, cut off to 140 characters, etc.)
+        let maxLenOfTweet = 140
         let hashtag = ConfigurationManager.AppSettings.["hashtag"]
+        let lead = "..."
+        let maxLenOfMsg = maxLenOfTweet - (hashtag + " \"\"").Length
         let msg  = Regex.Replace(nextTweetTo.Free_Comment, @"[ \t\r\n]", "")
-        let msg' = msg.Substring(0, min (msg.Length) (140 - 1 - hashtag.Length)) + " " + hashtag
+        let msg' = if msg.Length > maxLenOfMsg 
+                    then msg.Substring(0, maxLenOfMsg - lead.Length) + lead
+                    else msg
+        let fullmsg = "\"" + msg' + "\" " + hashtag
 
+        // Tweet!
         let key = ConfigurationManager.AppSettings.["twitterKey"]
         let tokens = JsonConvert.DeserializeObject<OAuthTokens>(key)
-        TwitterStatus.Update(tokens, msg') |> ignore
-
-
-
+        TwitterStatus.Update(tokens, fullmsg) |> ignore
